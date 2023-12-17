4d59859dce1c4da14b4004039bd2454906975362
Gaurav7112001 <153951105+Gaurav7112001@users.noreply.github.com>
2023-12-17 10:48:01 +0000

    container config done

---
+++ 01_basices/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3)//we are udong node.js not brower
+console.log(3+3)
+console.log("gaurav")
+
+
+let name ="gaurav"//string
+let age =18 //numberic
+let isloggedIn=false
+
+//number =>2 to power 53
+//bigint
+
+//sting=>""
+
+//boolean=>true/false
+//null=>standalone value
+//undifine =>
+//symbol=>unique
+
+
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
+
