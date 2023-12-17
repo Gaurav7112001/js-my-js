6b3699dad17275b2ed3318cdc0e21744b5c08ec9
Gaurav7112001 <153951105+Gaurav7112001@users.noreply.github.com>
2023-12-17 12:47:33 +0000

    container config done

---
+++ 01_basices/01_variable.js
@@ -0,0 +1,20 @@
+const accountId =14453
+let accountEmail="cse20201004@gamil.com"
+var accountPassword ="12345"
+accountCity ="patna"
+let accountState;
+
+// accountId=2 //not allowed
+
+
+accountEmail="hc@c.com"
+accountPassword ="1287"
+accountCity ="jaipur"
+
+console.log(accountId);
+/*
+prefer to not to use var
+because of issues of block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basices/03_conversion.js
@@ -0,0 +1,14 @@
+
+
+let isloggedIn =1
+
+let booleanisloggedIn =Boolean(isloggedIn)
+console.log(typeof booleanisloggedIn)
+console.log(booleanisloggedIn)
+//1 =>true;0=>false
+//"=false";"gaurav=>true"
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
