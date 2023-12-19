34f3c78b247ed3809ba1a533085120e1bf2ed8bf
Gaurav7112001 <153951105+Gaurav7112001@users.noreply.github.com>
2023-12-19 14:40:52 +0000

    container config done

--- 01_basices/03_conversion.js
+++ 01_basices/03_conversion.js
@@ -1,14 +1,53 @@
 
 
-let isloggedIn =1
+// let isloggedIn =1
 
-let booleanisloggedIn =Boolean(isloggedIn)
-console.log(typeof booleanisloggedIn)
-console.log(booleanisloggedIn)
-//1 =>true;0=>false
-//"=false";"gaurav=>true"
-let someNumber =33
+// let booleanisloggedIn =Boolean(isloggedIn)
+// console.log(typeof booleanisloggedIn)
+// console.log(booleanisloggedIn)
+// //1 =>true;0=>false
+// //"=false";"gaurav=>true"
+// let someNumber =33
 
-let stringNumber =String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber =String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+//*******************8opeeration****************** */
+let vlaue=3
+// let negvalue=-vlaue
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log( 2*2)
+// console.log(2**4)
+// console.log(2%3)
+// console.log(2/3)
+
+// let str1 ="gaueav"
+// let str2 =" kumar"
+// let str3=str1+str2
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+2+"4")
+// console.log(2+2*6%3)
+
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
+
+
+
+
+
+
+
+
---
+++ 01_basices/04_comparisiom.js
@@ -0,0 +1,47 @@
+// console.log("gaurav")
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined ==0)
+// console.log(undefined <0)
+// console.log(undefined >0)
+// console.log(undefined ==0)
+// console.log(2===2)
+
+
+
+// const id = Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id)
+// console.log(anotherId)
+// console.log(id==anotherId)
+const bignumber=111111111111111n
+
+
+const heros =["shaktiman","naagraj","doga"]
+let myobj={
+    name:"gaurav",
+    age:22,
+}
+const myFunction =function(){
+    console.log("hello gaurav")
+}
+
+console.log(typeof bignumber)
+
+
+
+
+
+
+
